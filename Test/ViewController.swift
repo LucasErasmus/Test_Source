96a30f00b4cb432583a91f710adfb9ac742d4d71
Lucas Erasmus <erasmuslb@ufs.ac.za>
2020-04-10 21:50:28 +0200

    Adds two numbers

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -15,7 +15,15 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    /**
+     Adds twp numbers together and returns the result.
+     - parameter num1: The first number.
+     - parameter num2: The second number.
+     - returns: The sum of num 1 and num 2.
+     */
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
