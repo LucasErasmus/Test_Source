cdb2f7e113f09b5fa8377b523089f4d6096590a7
Lucas Erasmus <erasmuslb@ufs.ac.za>
2020-04-10 21:24:02 +0200

    Initial Commit

---
+++ Test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Test
+//
+//  Created by Lucas Johannes Bartel Erasmus on 2020/04/10.
+//  Copyright © 2020 Lucas Johannes Bartel Erasmus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Lucas Johannes Bartel Erasmus on 2020/04/10.
+//  Copyright © 2020 Lucas Johannes Bartel Erasmus. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
